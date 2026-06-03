e188adaf60a4e1dfac3515d9c1df325a3bb85637
Arjun Kumar Dubey <reboostify@gmail.com>
2026-06-04 03:28:36 +0530

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   output: 'static',
   site: 'https://arjundubey.com',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
